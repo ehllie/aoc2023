0b0f8912133a18bb93b6db1ee2338bd6fb605989
Elizabeth Paź <me@ehllie.xyz>
2023-12-09 15:53:00 +0100

    Add day 9 solution

--- src/solutions/d09.rs
+++ src/solutions/d09.rs
@@ -1,7 +1,42 @@
 pub fn part_one(input: &str) -> String {
-    unimplemented!()
+    parse(input)
+        .map(|hist| {
+            derivatives(hist)
+                .into_iter()
+                .rev()
+                .fold(0, |acc, drv| acc + drv.last().unwrap())
+        })
+        .sum::<isize>()
+        .to_string()
 }
 
 pub fn part_two(input: &str) -> String {
-    unimplemented!()
+    parse(input)
+        .map(|hist| {
+            derivatives(hist)
+                .into_iter()
+                .rev()
+                .fold(0, |acc, drv| drv.first().unwrap() - acc)
+        })
+        .sum::<isize>()
+        .to_string()
+}
+
+fn derivatives<I: Iterator<Item = isize>>(iter: I) -> Vec<Vec<isize>> {
+    let mut derivatives = vec![iter.collect::<Vec<_>>()];
+    while !derivatives.last().unwrap().iter().all(|n| n == &0) {
+        let derivative = derivatives
+            .last()
+            .unwrap()
+            .windows(2)
+            .map(|win| win[1] - win[0]);
+        derivatives.push(derivative.collect());
+    }
+    derivatives
+}
+
+fn parse(input: &str) -> impl Iterator<Item = impl Iterator<Item = isize> + '_> + '_ {
+    input
+        .lines()
+        .map(|l| l.split(' ').filter_map(|s| s.parse().ok()))
 }
