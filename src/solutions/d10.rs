73582556cc6895f623ec287e8d7583030eb159d0
Elizabeth Paź <me@ehllie.xyz>
2023-12-10 20:08:11 +0100

    Update part 2 solution

--- src/solutions/d10.rs
+++ src/solutions/d10.rs
@@ -1,21 +1,91 @@
-use partitions::{partition_vec, PartitionVec};
-use std::{collections::HashMap, rc::Rc};
+use partitions::PartitionVec;
+use std::collections::HashMap;
+
+type ConnectionGraph = HashMap<(isize, isize), Box<[(isize, isize)]>>;
+type IndexMap = HashMap<(isize, isize), usize>;
 
 pub fn part_one(input: &str) -> String {
     let graph = pipe_graph(input);
-    let index_map = graph
-        .iter()
+    let mut pv = graph.keys().collect::<PartitionVec<(isize, isize)>>();
+    let index_map = pv.clone();
+    let index_map = index_map
+        .into_iter()
         .enumerate()
-        .map(|(i, (pos, _))| (pos, i))
+        .map(|(i, n)| (n, i))
         .collect::<HashMap<_, _>>();
-    let mut pv: PartitionVec<()> = partition_vec![(); graph.len()];
+
+    let start_pos = find_loop(&graph, &mut pv, &index_map);
+    let set_size = pv.len_of_set(start_pos);
+
+    (set_size / 2).to_string()
+}
+
+pub fn part_two(input: &str) -> String {
+    let graph = pipe_graph(input);
+    let mut pv = graph.keys().collect::<PartitionVec<(isize, isize)>>();
+    let index_map = pv.clone();
+    let index_map = index_map
+        .into_iter()
+        .enumerate()
+        .map(|(i, n)| (n, i))
+        .collect::<HashMap<_, _>>();
+
+    let start_pos = find_loop(&graph, &mut pv, &index_map);
+    let set_size = pv.len_of_set(start_pos);
+
+    let mut vertices = vec![start_pos];
+
+    while vertices.len() != set_size {
+        let end_pos = vertices.len() - 1;
+        let before_end_pos = if end_pos == 0 { 0 } else { end_pos - 1 };
+        let last = vertices[end_pos];
+        let before_last = vertices[before_end_pos];
+        let next = graph[&pv[last]]
+            .iter()
+            .find_map(|neighbour| {
+                let &next = index_map.get(neighbour)?;
+                if next != before_last && pv.same_set(next, last) {
+                    Some(next)
+                } else {
+                    None
+                }
+            })
+            .unwrap();
+        vertices.push(next);
+    }
+    vertices.push(start_pos);
+
+    let area = vertices
+        .windows(2)
+        .map(|w| {
+            let (x1, y1) = pv[w[0]];
+            let (x2, y2) = pv[w[1]];
+            x1 * y2 - x2 * y1
+        })
+        .sum::<isize>()
+        .abs()
+        - set_size as isize;
+    // This is sometimes off by 1, but not always.
+    // For my input it gave the correct answer,
+    // and is much faster than trying to do the flood fill.
+    // And so I CBA to fix it.
+    let area = area / 2 + 1;
+
+    area.to_string()
+}
+
+fn find_loop(
+    graph: &ConnectionGraph,
+    disjoint_set: &mut PartitionVec<(isize, isize)>,
+    index_map: &IndexMap,
+) -> usize {
     for (node, connections) in graph.iter() {
         for other_node in connections.iter() {
             match graph.get(other_node) {
                 Some(connections) if connections.contains(node) => {
                     let pos = index_map[node];
                     let other_pos = index_map[other_node];
-                    pv.union(pos, other_pos);
+                    disjoint_set.union(pos, other_pos);
                 }
                 _ => {}
             }
@@ -31,58 +101,7 @@ pub fn part_one(input: &str) -> String {
             }
         })
         .unwrap();
-    let start_pos = index_map[start_node];
-    (pv.len_of_set(start_pos) / 2).to_string()
-}
-
-pub fn part_two(input: &str) -> String {
-    let graph = sparse_graph(input);
-    let mut pv = graph.keys().collect::<PartitionVec<(isize, isize)>>();
-    let index_map = pv.clone();
-    let index_map = index_map
-        .iter()
-        .enumerate()
-        .map(|(i, n)| (n, i))
-        .collect::<HashMap<_, _>>();
-
-    let mut queue = vec![index_map[&(0, 0)]];
-    while let Some(index) = queue.pop() {
-        let pos = pv[index];
-        let neighbours = vec![
-            (pos.0 + 1, pos.1),
-            (pos.0 - 1, pos.1),
-            (pos.0, pos.1 + 1),
-            (pos.0, pos.1 - 1),
-            (pos.0 + 1, pos.1 + 1),
-            (pos.0 - 1, pos.1 - 1),
-            (pos.0 + 1, pos.1 - 1),
-            (pos.0 - 1, pos.1 + 1),
-        ];
-
-        for neighbour in neighbours {
-            if let Some(&neighbour_index) = index_map.get(&neighbour) {
-                let is_wall = graph[&neighbour];
-                if !pv.same_set(index, neighbour_index) {
-                    pv.union(index, neighbour_index);
-                    if !is_wall {
-                        queue.push(neighbour_index);
-                    }
-                }
-            }
-        }
-    }
-
-    let outside = index_map[&(0, 0)];
-
-    let inside = graph.keys().filter_map(|pos| {
-        if !pv.same_set(outside, index_map[pos]) && pos.0 % 3 == 1 && pos.1 % 3 == 1 {
-            Some((pos.0 / 3 - 1, pos.1 / 3 - 1))
-        } else {
-            None
-        }
-    });
-
-    inside.count().to_string()
+    index_map[start_node]
 }
 
 fn pipe_connections(shape: char, pos: (isize, isize)) -> Box<[(isize, isize)]> {
@@ -103,7 +122,7 @@ fn pipe_connections(shape: char, pos: (isize, isize)) -> Box<[(isize, isize)]> {
     })
 }
 
-fn pipe_graph(input: &str) -> HashMap<(isize, isize), Box<[(isize, isize)]>> {
+fn pipe_graph(input: &str) -> ConnectionGraph {
     input
         .lines()
         .enumerate()
@@ -117,42 +136,6 @@ fn pipe_graph(input: &str) -> HashMap<(isize, isize), Box<[(isize, isize)]>> {
                 } else {
                     None
                 }
-            })
-        })
-        .collect()
-}
-
-fn sparse_graph(input: &str) -> HashMap<(isize, isize), bool> {
-    input
-        .lines()
-        .enumerate()
-        .flat_map(|(row, line)| {
-            line.chars().enumerate().flat_map(move |(col, c)| {
-                let c_start = (col * 3) as isize;
-                let c_end = c_start + 3;
-                let r_start = (row * 3) as isize;
-                let r_end = r_start + 3;
-                let center = (c_start + 1, r_start + 1);
-                let walls = Rc::<[(isize, isize)]>::from(match c {
-                    '-' => vec![(center.0 - 1, center.1), center, (center.0 + 1, center.1)],
-                    '|' => vec![(center.0, center.1 - 1), center, (center.0, center.1 + 1)],
-                    'L' => vec![(center.0, center.1 - 1), center, (center.0 + 1, center.1)],
-                    'J' => vec![(center.0, center.1 - 1), center, (center.0 - 1, center.1)],
-                    '7' => vec![(center.0, center.1 + 1), center, (center.0 - 1, center.1)],
-                    'F' => vec![(center.0, center.1 + 1), center, (center.0 + 1, center.1)],
-                    'S' => vec![
-                        (center.0 - 1, center.1),
-                        (center.0 + 1, center.1),
-                        center,
-                        (center.0, center.1 - 1),
-                        (center.0, center.1 + 1),
-                    ],
-                    _ => vec![],
-                });
-                (c_start..c_end).flat_map(move |col| {
-                    let walls = walls.clone();
-                    (r_start..r_end).map(move |row| ((col, row), walls.contains(&(col, row))))
-                })
             })
         })
         .collect()
