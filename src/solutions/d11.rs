72d75fd590871608483db1b47088f227b7b8dc6a
Elizabeth Paź <me@ehllie.xyz>
2023-12-11 11:19:07 +0100

    Further optimise day 11

--- src/solutions/d11.rs
+++ src/solutions/d11.rs
@@ -1,32 +1,27 @@
 use std::collections::HashMap;
 
-type Galaxies = Vec<((isize, isize), (isize, isize))>;
+type Galaxies = Vec<(isize, isize)>;
 
 pub fn part_one(input: &str) -> String {
-    let galaxies = parse_universe(input);
-    galaxy_distances(&galaxies, 2).to_string()
+    let galaxies = parse_universe(input, 2);
+    galaxy_distances(&galaxies).to_string()
 }
 
 pub fn part_two(input: &str) -> String {
-    let galaxies = parse_universe(input);
-    galaxy_distances(&galaxies, 1_000_000).to_string()
+    let galaxies = parse_universe(input, 1_000_000);
+    galaxy_distances(&galaxies).to_string()
 }
 
-fn galaxy_distances(galaxies: &Galaxies, expansion_rate: isize) -> isize {
-    let expansion_rate = expansion_rate - 1;
-    galaxies.iter().enumerate().fold(0, |acc, (i, (g1, e1))| {
-        galaxies.iter().skip(i + 1).fold(acc, |acc, (g2, e2)| {
-            let e1 = (e1.0 * expansion_rate, e1.1 * expansion_rate);
-            let e2 = (e2.0 * expansion_rate, e2.1 * expansion_rate);
-            let (x1, y1) = (g1.0 + e1.0, g1.1 + e1.1);
-            let (x2, y2) = (g2.0 + e2.0, g2.1 + e2.1);
+fn galaxy_distances(galaxies: &Galaxies) -> isize {
+    galaxies.iter().enumerate().fold(0, |acc, (i, (x1, y1))| {
+        galaxies.iter().skip(i + 1).fold(acc, |acc, (x2, y2)| {
             let manhatan = (x1 - x2).abs() + (y1 - y2).abs();
             acc + manhatan
         })
     })
 }
 
-fn parse_universe(input: &str) -> Galaxies {
+fn parse_universe(input: &str, expansion_rate: isize) -> Galaxies {
     let mut columns = HashMap::<isize, Vec<(isize, isize, isize)>>::new();
     let mut expanded_rows = 0;
     let mut row_length = 0;
@@ -59,11 +54,13 @@ fn parse_universe(input: &str) -> Galaxies {
         }
     }
 
+    let expansion_rate = expansion_rate - 1;
     columns
         .into_iter()
         .flat_map(|(col, rows)| {
-            rows.into_iter()
-                .map(move |(row, e_col, e_row)| ((col, row), (e_col, e_row)))
+            rows.into_iter().map(move |(row, e_col, e_row)| {
+                (col + e_col * expansion_rate, row + e_row * expansion_rate)
+            })
         })
         .collect()
 }
